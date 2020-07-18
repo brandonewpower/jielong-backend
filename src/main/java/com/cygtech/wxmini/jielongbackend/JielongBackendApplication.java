92e1cbf12542594d2718ada6698cfc8963babd00
billwenboli <zhlwbl@gmail.com>
2020-07-18 13:44:10 -0700

    first commit

---
+++ src/main/java/com/cygtech/wxmini/jielongbackend/JielongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cygtech.wxmini.jielongbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JielongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JielongBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cygtech/wxmini/jielongbackend/JielongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cygtech.wxmini.jielongbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JielongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
